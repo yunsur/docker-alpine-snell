4166d262638f7c0593b007cdd44857addec1c870
yunsur <yunsur.shi@gmail.com>
2021-02-12 18:51:24 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM yunsur/alpine-glibc
+
+ARG SNELL_SERVER_VERSION=v2.0.3
+ARG SNELL_SERVER_PACKAGE=https://github.com/surge-networks/snell/releases/download/${SNELL_SERVER_VERSION}/snell-server-${SNELL_SERVER_VERSION}-linux-amd64.zip
+
+ENV LANG=C.UTF-8
+ENV PORT=443
+ENV PSK=
+ENV OBFS=tls
+
+COPY entrypoint.sh /usr/bin/
+
+RUN wget --no-check-certificate -O snell.zip $SNELL_SERVER_PACKAGE && \
+    unzip snell.zip && \
+    rm -f snell.zip && \
+    chmod +x snell-server && \
+    mv snell-server /usr/bin/ && \
+    chmod +x /usr/bin/entrypoint.sh
+
+ENTRYPOINT ["entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/ash
+
+BIN="/usr/bin/snell-server"
+CONF="/etc/snell/snell-server.conf"
+
+# reuse existing config when the container restarts
+run() {
+    if [ -f ${CONF} ]; then
+        echo "Found existing config..."
+    else
+    if [ -z ${PSK} ]; then
+        PSK=$(hexdump -n 16 -e '4/4 "%08x" 1 "\n"' /dev/urandom)
+        echo "Using generated PSK: ${PSK}"
+    else
+        echo "Using predefined PSK: ${PSK}"
+    fi
+        echo "Generating new config..."
+        mkdir /etc/snell/
+        echo "[snell-server]" >> ${CONF}
+        echo "listen = 0.0.0.0:${PORT}" >> ${CONF}
+        echo "psk = ${PSK}" >> ${CONF}
+        echo "obfs = ${OBFS}" >> ${CONF}
+    fi
+    ${BIN} -c ${CONF}
+}
+
+run
